69548f4a7f548e5aeb51a1c4a542f1d0477860bf
Killian Raoux <killian@semifir.com>
2023-05-11 09:10:01 +0200

    first commit

---
+++ src/test/java/fr/kira/formation/demo/MesTest.java
@@ -0,0 +1,16 @@
+package fr.kira.formation.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class MesTest {
+
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
+
+//    @Test
+    public void fail() {
+        assert false;
+    }
+}
